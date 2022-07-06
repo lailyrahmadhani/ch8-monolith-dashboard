ad494bcf9fa62c20bec162c0a540d0a9d05459e1
Laily Rahmadhani <39314731+lailyrahmadhani@users.noreply.github.com>
2022-07-07 06:59:39 +0700

    initial commit

---
+++ controller/auth/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    login: (req, res) => {
+      res.render("pages/authentication/login", {
+        layout: "layouts/authentication",
+      });
+    },
+  
+    register: (req, res) => {
+      res.render("pages/authentication/register", {
+        layout: "layouts/authentication",
+      });
+    },
+  
+    api: require("./api"),
+  };
+  
